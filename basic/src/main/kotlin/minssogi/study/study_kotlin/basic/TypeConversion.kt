d6b9c4b31c3c0c8958418c3367c970db4ac71592
Mingsicogi <codedoctor119@gmail.com>
2022-07-28 20:48:07 +0900

    implicit vs explicit of number type

---
+++ basic/src/main/kotlin/minssogi/study/study_kotlin/basic/TypeConversion.kt
@@ -0,0 +1,10 @@
+package minssogi.study.study_kotlin.basic
+
+fun main() {
+
+    // int to long
+    val intNumber = 10
+    println("${intNumber::class.java}")
+    println("${intNumber.toLong()::class.java}")
+
+}
