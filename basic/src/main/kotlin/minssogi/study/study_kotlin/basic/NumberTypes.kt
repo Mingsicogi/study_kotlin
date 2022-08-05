84962f960bbfcc7998a14222aa80f83226813eaf
Mingsicogi <codedoctor119@gmail.com>
2022-08-05 20:19:12 +0900

    user input integer

---
+++ basic/src/main/kotlin/minssogi/study/study_kotlin/basic/NumberTypes.kt
@@ -0,0 +1,9 @@
+package minssogi.study.study_kotlin.basic
+
+fun main() {
+
+    println(10::class.java)
+    println(700_000_000_000::class.java)
+    println(10.0::class.java)
+    println(10.000000::class.java)
+}
