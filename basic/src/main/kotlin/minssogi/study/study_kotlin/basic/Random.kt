cd9ad3385465aff35f61ca6e39ab161acc3f3133
Mingsicogi <codedoctor119@gmail.com>
2022-08-08 20:09:35 +0900

    user input integer

--- basic/src/main/kotlin/minssogi/study/study_kotlin/basic/Random.kt
+++ basic/src/main/kotlin/minssogi/study/study_kotlin/basic/Random.kt
@@ -7,4 +7,5 @@ fun main() {
 
     println("random lib : ${Random.nextInt()}")
     println("random lib : ${Random.nextInt(1, 11)}")
+    println("random lib : ${Random.nextDouble(1.0000, 2.000)}")
 }
