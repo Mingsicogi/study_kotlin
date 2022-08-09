1b493162fce14c1762c4c288b59d097c9ce1cb73
Mingsicogi <codedoctor119@gmail.com>
2022-08-09 20:08:17 +0900

    user input integer

---
+++ basic/src/main/kotlin/minssogi/study/study_kotlin/basic/Random.kt
@@ -0,0 +1,10 @@
+package minssogi.study.study_kotlin.basic
+
+import kotlin.random.Random
+
+fun main() {
+
+
+    println("random lib : ${Random.nextInt()}")
+    println("random lib : ${Random.nextInt(1, 11)}")
+}
