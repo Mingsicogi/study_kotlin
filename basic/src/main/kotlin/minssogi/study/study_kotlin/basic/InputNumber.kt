bc6181d8ac8046a27fa571fd98e0a9306fa34ccc
Mingsicogi <codedoctor119@gmail.com>
2022-08-10 20:07:13 +0900

    user input integer

--- basic/src/main/kotlin/minssogi/study/study_kotlin/basic/InputNumber.kt
+++ basic/src/main/kotlin/minssogi/study/study_kotlin/basic/InputNumber.kt
@@ -2,7 +2,7 @@ package minssogi.study.study_kotlin.basic
 
 fun main() {
     println("Input number : ")
-    val inputNumber = readLine()
+    val inputNumber = readLine()?:"".toInt()
 
     println("You input $inputNumber")
 }
