a09b8c1506c29d2d245366ec0ddbb36dc36e4674
Mingsicogi <codedoctor119@gmail.com>
2022-08-24 18:29:41 +0900

    Run function

---
+++ basic/src/main/kotlin/minssogi/study/study_kotlin/basic/RunFunction.kt
@@ -0,0 +1,18 @@
+package minssogi.study.study_kotlin.basic
+
+fun main() {
+
+    Car(80, "minssogi car").run {
+        println("car info : car name = ${this.name} | speed = ${this.speed}")
+    }
+}
+
+class Car(speed: Int, name: String) {
+    internal var speed = 0
+    internal var name = ""
+
+    init {
+        this.speed = speed
+        this.name = name
+    }
+}
