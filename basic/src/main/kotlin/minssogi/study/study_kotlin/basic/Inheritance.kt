752bc41652c84e2710e707982943ef358b44baf5
전민석 <codedoctor119@gmail.com>
2022-08-26 16:41:25 +0900

    Inheritance

---
+++ basic/src/main/kotlin/minssogi/study/study_kotlin/basic/Inheritance.kt
@@ -0,0 +1,22 @@
+package minssogi.study.study_kotlin.basic
+
+fun main() {
+    val son = Son("Mins", 123)
+
+    son.printPrivacyInfo()
+}
+
+class Son(private val name: String, private val age: Int): Mom("Jane", 50) {
+
+    override fun printPrivacyInfo() {
+        super.printPrivacyInfo()
+        println("Son name: $name, age: $age")
+    }
+}
+
+open class Mom(private val name: String, private val age: Int) {
+
+    open fun printPrivacyInfo() {
+        println("Mom name : $name, age : $age")
+    }
+}
