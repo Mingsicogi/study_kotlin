4b4be9b4570b871100adf7b3ab05f8ea5894d197
Mingsicogi <codedoctor119@gmail.com>
2022-11-19 20:34:31 +0900

    add coroutine study code

---
+++ coroutine/src/test/kotlin/minssogi/study/study_kotlin/coroutine/CoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package minssogi.study.study_kotlin.coroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
